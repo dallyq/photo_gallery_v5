d86e89dcfe68c8b76169c6f756560b4c53d45899
Dallas Quin <166860264+dallyq@users.noreply.github.com>
2026-01-09 16:12:12 +0200

    Changed author variable name to 'captions'.

--- js/searchFilter.js
+++ js/searchFilter.js
@@ -3,11 +3,11 @@ imageSearch.addEventListener('keyup', e => {
     let currentValue = e.target.value.toLowerCase(); 
     console.log(currentValue);
     let authors = document.querySelectorAll('.gallery a'); 
-    authors.forEach(author => { 
-        if (author.getAttribute('data-caption').toLowerCase().includes(currentValue)) { 
-            author.parentNode.style.display = 'block'; 
+    authors.forEach(captions => { 
+        if (captions.getAttribute('data-caption').toLowerCase().includes(currentValue)) { 
+            captions.parentNode.style.display = 'block'; 
         } else { 
-            author.parentNode.style.display = 'none'; 
+            captions.parentNode.style.display = 'none'; 
         } 
     }); 
 });
